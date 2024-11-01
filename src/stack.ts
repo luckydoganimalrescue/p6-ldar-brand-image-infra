bac769085df62a7b3a1f3a321b3e9243e9c11da4
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-11-01 16:47:19 -0500

    feat: recombine api/infra (#2)

--- src/stack.ts
+++ src/stack.ts
@@ -1,71 +1,50 @@
 import type { Construct } from 'constructs'
-import * as path from 'node:path'
 import * as cdk from 'aws-cdk-lib'
 import * as apigw from 'aws-cdk-lib/aws-apigatewayv2'
 import * as apigwi from 'aws-cdk-lib/aws-apigatewayv2-integrations'
 import * as certificatemanager from 'aws-cdk-lib/aws-certificatemanager'
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront'
 import * as cloudfront_origins from 'aws-cdk-lib/aws-cloudfront-origins'
+import * as iam from 'aws-cdk-lib/aws-iam'
 import * as lambda from 'aws-cdk-lib/aws-lambda'
 import * as lambdajs from 'aws-cdk-lib/aws-lambda-nodejs'
 import * as route53 from 'aws-cdk-lib/aws-route53'
+import * as route53Patterns from 'aws-cdk-lib/aws-route53-patterns'
 import * as route53targets from 'aws-cdk-lib/aws-route53-targets'
 import * as s3 from 'aws-cdk-lib/aws-s3'
-import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment'
-
 import * as ses from 'aws-cdk-lib/aws-ses'
 import * as floyd from 'cdk-iam-floyd'
 
-const HOSTED_ZONE_NAME = 'p6m7g8.net'
-const VERIFY_EMAIL = `pgollucci@${HOSTED_ZONE_NAME}`
-const SUBDOMAIN_NAME = 'api.ldar'
-const FROM_EMAIL = `ldar-pet-brander@p6m7g8.com`
-const RECORD_NAME = `${SUBDOMAIN_NAME}.${HOSTED_ZONE_NAME}`
-const CLOUDFRONT_RECORD_NAME = `www.ldar` + `.${HOSTED_ZONE_NAME}`
+export interface IStackProps extends cdk.StackProps {
+  hostedZoneName: string
+  verifyEmail: string
+  apiRecordName: string
+  fromEmail: string
+  cloudfrontRecordName: string
+  cloudfrontDomainName: string
+}
 
 export class MyStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props: cdk.StackProps = {}) {
+  constructor(scope: Construct, id: string, props: IStackProps) {
     super(scope, id, props)
 
+    // Lookup for the hosted zone
     const hostedZone = route53.HostedZone.fromLookup(this, 'HostedZone', {
-      domainName: HOSTED_ZONE_NAME,
+      domainName: props.hostedZoneName,
     })
 
-    const api_certificate = new certificatemanager.Certificate(
-      this,
-      'Certificate',
-      {
-        domainName: RECORD_NAME,
-        validation: certificatemanager.CertificateValidation.fromEmail({
-          email: VERIFY_EMAIL,
-        }),
-      },
-    )
-    const www_certificate = new certificatemanager.Certificate(
-      this,
-      'WWW-Certificate',
-      {
-        domainName: CLOUDFRONT_RECORD_NAME,
-        validation: certificatemanager.CertificateValidation.fromEmail({
-          email: VERIFY_EMAIL,
-        }),
-      },
-    )
-
-    const domainName = new apigw.DomainName(this, 'DN', {
-      domainName: RECORD_NAME,
-      certificate: api_certificate,
+    // Create the certificate for the domain
+    const certificate = new certificatemanager.Certificate(this, 'WWWCertificate', {
+      domainName: props.cloudfrontRecordName,
+      validation: certificatemanager.CertificateValidation.fromEmail({
+        email: props.verifyEmail,
+      }),
     })
 
-    // eslint-disable-next-line no-new
-    new ses.EmailIdentity(this, 'Identity', {
-      identity: ses.Identity.email(FROM_EMAIL),
-    })
-
-    const bucket = new s3.Bucket(this, 'MyBucket', {
+    // Define the S3 bucket for website hosting
+    const site_bucket = new s3.Bucket(this, 'MyBucket', {
       accessControl: s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
-      publicReadAccess: true,
+      publicReadAccess: true, // Required for static website hosting to work with CloudFront
       websiteIndexDocument: 'index.html',
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       cors: [
@@ -76,35 +55,133 @@ export class MyStack extends cdk.Stack {
           maxAge: 3000,
         },
       ],
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS, // Allows public access via bucket policy
     })
-    const oai = new cloudfront.OriginAccessIdentity(this, 'OAI')
-    const distribution = new cloudfront.Distribution(this, 'Distribution', {
-      defaultBehavior: {
-        origin: new cloudfront_origins.S3Origin(bucket, {
-          originAccessIdentity: oai, // If you're using an Origin Access Identity
-        }),
-        allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+
+    const logBucket = new s3.Bucket(this, 'LogBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      publicReadAccess: false,
+      accessControl: s3.BucketAccessControl.LOG_DELIVERY_WRITE, // Enable ACLs for CloudFront logging
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS, // Allow ACLs for logging while blocking public access
+    })
+
+    // Grant CloudFront permission to write logs to the log bucket
+    logBucket.addToResourcePolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: ['s3:PutObject'],
+      resources: [logBucket.arnForObjects('*')],
+      principals: [new iam.ServicePrincipal('delivery.logs.amazonaws.com')],
+      conditions: {
+        StringEquals: {
+          'aws:SourceAccount': cdk.Aws.ACCOUNT_ID,
+        },
+        ArnLike: {
+          'aws:SourceArn': `arn:aws:cloudfront::${cdk.Aws.ACCOUNT_ID}:distribution/*`,
+        },
       },
-      domainNames: [CLOUDFRONT_RECORD_NAME],
-      certificate: www_certificate,
+    }))
+
+    const cachePolicy = new cloudfront.CachePolicy(this, 'CachePolicy', {
+      defaultTtl: cdk.Duration.days(1),
+      minTtl: cdk.Duration.seconds(0),
+      maxTtl: cdk.Duration.days(365),
+      cookieBehavior: cloudfront.CacheCookieBehavior.none(),
+      headerBehavior: cloudfront.CacheHeaderBehavior.none(),
+      queryStringBehavior: cloudfront.CacheQueryStringBehavior.none(),
+      enableAcceptEncodingGzip: true,
+      enableAcceptEncodingBrotli: true,
+    })
+
+    const distribution = new cloudfront.Distribution(this, 'Distribution', {
+      defaultRootObject: 'index.html',
+      comment: props.cloudfrontRecordName,
+      enabled: true,
+      priceClass: cloudfront.PriceClass.PRICE_CLASS_100,
+      domainNames: [props.cloudfrontRecordName], // Use only one domain to avoid conflicts
+      certificate,
       minimumProtocolVersion: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021,
+      defaultBehavior: {
+        origin: new cloudfront_origins.S3StaticWebsiteOrigin(site_bucket),
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_GET_HEAD,
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        cachePolicy,
+      },
+      enableLogging: true,
+      logBucket,
+      logIncludesCookies: true,
+    })
+
+    const cloudfrontTarget = route53.RecordTarget.fromAlias(
+      new route53targets.CloudFrontTarget(distribution),
+    )
+
+    // Create both A (IPv4) and AAAA (IPv6) DNS records for www.ldar.p6m7g8.org (cloudfrontRecordName)
+    new route53.ARecord(this, 'CloudfrontDnsRecordWWW', {
+      zone: hostedZone,
+      recordName: props.cloudfrontRecordName,
+      target: cloudfrontTarget,
+    })
+
+    new route53.AaaaRecord(this, 'CloudfrontDnsRecordAAAAWWW', {
+      zone: hostedZone,
+      recordName: props.cloudfrontRecordName,
+      target: cloudfrontTarget,
+    })
+
+    // Use HttpsRedirect for redirecting root domain (hostedZoneName) to www
+    new route53Patterns.HttpsRedirect(this, 'Redirect', {
+      recordNames: [props.cloudfrontDomainName], // Redirect root domain
+      targetDomain: props.cloudfrontRecordName, // Redirect to www
+      zone: hostedZone,
+    })
+
+    // -----------------------------------------------------------------------
+
+    const api_certificate = new certificatemanager.Certificate(
+      this,
+      'APICertificate',
+      {
+        domainName: props.apiRecordName,
+        validation: certificatemanager.CertificateValidation.fromEmail({
+          email: props.verifyEmail,
+        }),
+      },
+    )
+
+    new ses.EmailIdentity(this, 'Identity', {
+      identity: ses.Identity.email(props.fromEmail),
+    })
+
+    const bucket = new s3.Bucket(this, 'ImageBucket', {
+      accessControl: s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
+      publicReadAccess: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      cors: [
+        {
+          allowedMethods: [s3.HttpMethods.GET, s3.HttpMethods.PUT],
+          allowedOrigins: ['*'],
+          allowedHeaders: ['*'],
+          maxAge: 3000,
+        },
+      ],
     })
 
     const presignFunc = new lambdajs.NodejsFunction(this, 'presign', {
       runtime: lambda.Runtime.NODEJS_20_X,
-      entry: path.join(__dirname, '../../api/src/presign.ts'),
       handler: 'handler',
       bundling: {
         minify: true,
       },
     })
+
     bucket.grantPut(presignFunc)
     bucket.grantPublicAccess('*', 's3:PutObject')
 
     presignFunc.addEnvironment('BUCKET_NAME', bucket.bucketName)
     const brandFunc = new lambdajs.NodejsFunction(this, 'brand', {
       runtime: lambda.Runtime.NODEJS_20_X,
-      entry: path.join(__dirname, '../../api/src/brand.ts'),
       handler: 'handler',
       tracing: lambda.Tracing.ACTIVE,
       timeout: cdk.Duration.minutes(14),
@@ -119,8 +196,8 @@ export class MyStack extends cdk.Stack {
     bucket.grantReadWrite(brandFunc)
 
     brandFunc.addEnvironment('BRAND_IMAGE_BUCKET', bucket.bucketName)
-    brandFunc.addEnvironment('EMAIL_SENDER', FROM_EMAIL)
-    brandFunc.addEnvironment('EMAIL_REGION', 'us-east-1')
+    brandFunc.addEnvironment('EMAIL_SENDER', props.fromEmail)
+    brandFunc.addEnvironment('EMAIL_REGION', this.region)
     brandFunc.addEnvironment('FIT', 'fill')
 
     const policy = new floyd.Statement.Ses()
@@ -128,6 +205,11 @@ export class MyStack extends cdk.Stack {
       .to('ses:SendEmail')
       .on('*')
     brandFunc.addToRolePolicy(policy)
+
+    const domainName = new apigw.DomainName(this, 'DN', {
+      domainName: props.apiRecordName,
+      certificate: api_certificate,
+    })
 
     const httpApi = new apigw.HttpApi(this, 'HttpApi', {
       description: 'Pet Brander API',
@@ -162,17 +244,6 @@ export class MyStack extends cdk.Stack {
       integration: brandIntegration,
     })
 
-    const cloudfrontTarget = route53.RecordTarget.fromAlias(
-      new route53targets.CloudFrontTarget(distribution),
-    )
-
-    // eslint-disable-next-line no-new
-    new route53.ARecord(this, 'CloudfrontDnsRecord', {
-      zone: hostedZone,
-      recordName: CLOUDFRONT_RECORD_NAME,
-      target: cloudfrontTarget,
-    })
-
     const apiGatewayTarget = route53.RecordTarget.fromAlias(
       new route53targets.ApiGatewayv2DomainProperties(
         domainName.regionalDomainName,
@@ -180,22 +251,10 @@ export class MyStack extends cdk.Stack {
       ),
     )
 
-    // eslint-disable-next-line no-new
     new route53.ARecord(this, 'DnsRecord', {
       zone: hostedZone,
-      recordName: RECORD_NAME,
+      recordName: props.apiRecordName,
       target: apiGatewayTarget,
-    })
-
-    const sourceAsset = s3deploy.Source.asset('../website/out')
-
-    // eslint-disable-next-line no-new
-    new s3deploy.BucketDeployment(this, 'DeployWithInvalidation', {
-      sources: [sourceAsset],
-      destinationBucket: bucket,
-      distribution,
-      distributionPaths: ['/*'],
-      prune: false,
     })
   }
 }
